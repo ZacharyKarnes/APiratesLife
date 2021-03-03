ddde2e93890c19da18eaccf37157588503c8af10
Zachary Karnes <zach.karnes@outlook.com>
2021-03-03 14:46:34 -0700

    Added basic Unity setup
    Base scripts
    Input scripts
    Default scenes

---
+++ Assets/InputHandler/Control.cs
@@ -0,0 +1,216 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Inputhandler/Control.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @Control : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @Control()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Control"",
+    ""maps"": [
+        {
+            ""name"": ""locomotion"",
+            ""id"": ""3144546b-f7be-49a9-ba52-27ec83605dd3"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""c56d0b48-bffa-46ca-8484-79c4cbdec2ac"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Camera"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""aa3cd4cc-2741-4e34-9d9a-d8738e0bd971"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""c9f69934-7df5-4f7d-843e-4dddf07bf9d8"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""4d34612c-a600-47ed-85d9-652d2974c0e2"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""8349644d-d143-4cb9-8689-c86cd50e2ee4"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""2ad82146-4915-4b39-af93-6908c7918aa6"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""7f548a0d-2b3d-4c74-8bf8-d652161a4e61"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""7eec26ce-dc5b-4d04-a2ed-f9dfc2f042dd"",
+                    ""path"": ""<Gamepad>/rightStick"",
+                    ""interactions"": """",
+                    ""processors"": ""StickDeadzone"",
+                    ""groups"": """",
+                    ""action"": ""Camera"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""634f177b-c73a-4ce6-bce7-c6b4f62e249d"",
+                    ""path"": ""<Mouse>/delta"",
+                    ""interactions"": """",
+                    ""processors"": ""NormalizeVector2"",
+                    ""groups"": """",
+                    ""action"": ""Camera"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // locomotion
+        m_locomotion = asset.FindActionMap("locomotion", throwIfNotFound: true);
+        m_locomotion_Move = m_locomotion.FindAction("Move", throwIfNotFound: true);
+        m_locomotion_Camera = m_locomotion.FindAction("Camera", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // locomotion
+    private readonly InputActionMap m_locomotion;
+    private ILocomotionActions m_LocomotionActionsCallbackInterface;
+    private readonly InputAction m_locomotion_Move;
+    private readonly InputAction m_locomotion_Camera;
+    public struct LocomotionActions
+    {
+        private @Control m_Wrapper;
+        public LocomotionActions(@Control wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_locomotion_Move;
+        public InputAction @Camera => m_Wrapper.m_locomotion_Camera;
+        public InputActionMap Get() { return m_Wrapper.m_locomotion; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(LocomotionActions set) { return set.Get(); }
+        public void SetCallbacks(ILocomotionActions instance)
+        {
+            if (m_Wrapper.m_LocomotionActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_LocomotionActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_LocomotionActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_LocomotionActionsCallbackInterface.OnMove;
+                @Camera.started -= m_Wrapper.m_LocomotionActionsCallbackInterface.OnCamera;
+                @Camera.performed -= m_Wrapper.m_LocomotionActionsCallbackInterface.OnCamera;
+                @Camera.canceled -= m_Wrapper.m_LocomotionActionsCallbackInterface.OnCamera;
+            }
+            m_Wrapper.m_LocomotionActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @Camera.started += instance.OnCamera;
+                @Camera.performed += instance.OnCamera;
+                @Camera.canceled += instance.OnCamera;
+            }
+        }
+    }
+    public LocomotionActions @locomotion => new LocomotionActions(this);
+    public interface ILocomotionActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnCamera(InputAction.CallbackContext context);
+    }
+}
